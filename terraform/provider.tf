b6d48ddcec1511b14811a62a881c25bf04c4d37d
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2023-05-07 02:20:25 +0200

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "721c9ae7-1ccf-4733-88b7-9cd6a3dcfab2"
+}
